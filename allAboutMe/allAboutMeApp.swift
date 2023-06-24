6aedb7b6a2f6abe470889678d678571439b6de80
Liz Hernandez <lizcarol2005@gmail.com>
2023-06-24 07:44:36 -0400

    Initial Commit

---
+++ allAboutMe/allAboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  allAboutMeApp.swift
+//  allAboutMe
+//
+//  Created by Tomas Hernandez on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct allAboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
